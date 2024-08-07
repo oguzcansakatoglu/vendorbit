77aeab7915b48e893070ca069716e773eee689c1
sakatogluoguzcan <oguzcan.sakatoglu@ekodenge.com>
2024-08-07 12:17:31 +0300

    init

--- src/utils/ThemeContext.tsx
+++ src/utils/ThemeContext.tsx
@@ -1,5 +1,13 @@
 import {ColorPalette, getColors} from './palette';
-import React, {ReactNode, createContext, useContext, useState} from 'react';
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type ThemeContextType = {
   isDarkMode: boolean;
@@ -11,11 +19,51 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider: React.FC<{children: ReactNode}> = ({children}) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const colors = getColors(isDarkMode ? 'dark' : 'light');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Load the theme preference when the component mounts
+    loadThemePreference();
+  }, []);
+
+  useEffect(() => {
+    // Save the theme preference whenever it changes
+    if (!isLoading) {
+      saveThemePreference();
+    }
+  }, [isDarkMode, isLoading]);
+
+  const loadThemePreference = async () => {
+    try {
+      const value = await AsyncStorage.getItem('isDarkMode');
+      if (value !== null) {
+        setIsDarkMode(value === 'true');
+      }
+    } catch (error) {
+      console.error('Error loading theme preference:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const saveThemePreference = async () => {
+    try {
+      await AsyncStorage.setItem('isDarkMode', isDarkMode.toString());
+    } catch (error) {
+      console.error('Error saving theme preference:', error);
+    }
+  };
 
   const toggleTheme = () => {
     setIsDarkMode(prev => !prev);
   };
+
+  const colors = getColors(isDarkMode ? 'dark' : 'light');
+
+  if (isLoading) {
+    // You might want to return a loading indicator here
+    return null;
+  }
 
   return (
     <ThemeContext.Provider value={{isDarkMode, toggleTheme, colors}}>
